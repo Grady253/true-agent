7c9789c36461fac54c36f086f33e01a1422f1f7b
Grady253 <grady.andre2@gmail.com>
2023-04-04 20:03:49 -0700

    Added post route.

--- Assests/server.js
+++ Assests/server.js
@@ -5,6 +5,7 @@ searchBtnEl.addEventListener('click', () => {
     const citySearch = citySearchEl.value;
     console.log(citySearch);
     getPropertyApi(citySearch);
+    postPropertyApi();
 });
 
 
@@ -17,9 +18,27 @@ function getPropertyApi(citySearch){
     }
   };
   
-  fetch(`https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=${citySearch}&limit=10`, location)
+  fetch(`https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=${citySearch}&limit=30`, location)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
 };
+
+
+function postPropertyApi(){
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': '44a9dd568emshc2dbe79a2be69f1p1adfdejsnec1e21b078cd',
+      'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
+    },
+    body: '{"limit":20,"offset":0,"postal_code":"90004","status":["for_sale","ready_to_build"],"sort":{"direction":"desc","field":"list_date"}}'
+  };
+  
+  fetch('https://realty-in-us.p.rapidapi.com/properties/v3/list', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
