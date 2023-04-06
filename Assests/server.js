11d5bf13e66dd040fcd3770239d1f99aa1344e79
unknown <grady.andre2@gmail.com>
2023-04-05 21:33:29 -0700

    Still working with Api fetch.

--- Assests/server.js
+++ Assests/server.js
@@ -1,14 +1,17 @@
 const searchBtnEl = document.querySelector('.search-btn');
 const citySearchEl = document.querySelector('#location');
+const searchList = document.querySelector('ul');
+
+const apiKey = '0c869b4faac810b2e55866781d64a8a6';
+
 
 searchBtnEl.addEventListener('click', () => {
     const citySearch = citySearchEl.value;
-    console.log(citySearch);
     getPropertyApi(citySearch);
 });
 
 
-function getPropertyApi(citySearch){
+async function getPropertyApi(){
 
   const options = {
     method: 'GET',
@@ -18,9 +21,16 @@ function getPropertyApi(citySearch){
     }
   };
   
-  fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=100&sort=relevance', options)
+  await fetch('https://realty-in-us.p.rapidapi.com/properties/list-for-sale?state_code=NY&city=New%20York%20City&offset=0&limit=20&sort=relevance', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then( response => { 
+      console.log(response);
+      for( let i = 0; i < response.listings[i].prop_type.length; i++){
+        const listItem = document.createElement('Li');
+        listItem.textContent = response.listings[i].prop_type;
+        searchList.appendChild(listItem);
+      };
+    })
     .catch(err => console.error(err));
 
 };
