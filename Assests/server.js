76a57cbc6ed798be05e66f17a03c224c3953e2ce
unknown <grady.andre2@gmail.com>
2023-04-11 22:25:49 -0700

    Got some data to render.

--- Assests/server.js
+++ Assests/server.js
@@ -1,48 +1,42 @@
 const searchBtnEl = document.querySelector(".search-btn");
-const citySearchEl = document.querySelector("#location");
-const propDisplayEl = document.querySelector("#property-display");
+const zipSearchEl = document.querySelector("#zipCode");
+const stateSearchEl = document.querySelector("#state");
+const propDisplayEl = document.querySelector(".property-display");
 const searchList = document.querySelector("ul");
 
-const apiKey = "0c869b4faac810b2e55866781d64a8a6";
+const apiKey = "44a9dd568emshc2dbe79a2be69f1p1adfdejsnec1e21b078cd";
 
 searchBtnEl.addEventListener("click", () => {
-  const citySearch = citySearchEl.value;
-  getPropertyApi(citySearch);
+  const zipCode = zipSearchEl.value;
+  getPropertyApi(zipCode);
 });
 
-async function getPropertyApi(citySearch) {
+async function getPropertyApi(zipCode) {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '44a9dd568emshc2dbe79a2be69f1p1adfdejsnec1e21b078cd',
-      'X-RapidAPI-Host': 'realty-in-us.p.rapidapi.com'
-    }
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+    },
   };
 
-  await fetch(`https://realty-in-us.p.rapidapi.com/properties/detail?listing_id=${citySearch}&prop_status=for_sale`, options)
-    .then(response => response.json())
-    .then( response => {
-      docFrag = document.createDocumentFragment();
-      console.log(response);
-      for( let i = 0; i < response.autocomplete[i].length; i++){
-        const divEl = document.createElement('div');
-        divEl.setAttribute("class", "box is-flex container");
+  await fetch(
+    `https://realty-in-us.p.rapidapi.com/agents/list?postal_code=${zipCode}&limit=10&types=agent`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => displayData(response))
+    .catch((err) => console.error(err));
+}
 
-        let template = `
-        <article >
-          <div class="content">
-            <img alt='property photo' src=${response.autocomplete[i].photo}/>
-              <h3>${response.autocomplete[i].price}</h3>
-              <h4>${response.autocomplete[i].address}</h4>
-          </div>
-        </article>
-        `;
-        divEl.innerHTML = template;
-        docFrag.append(divEl);
-      };
-
-    })
-    .catch(err => console.error(err));
-
-
+function displayData(data) {
+  console.log(data.agents);
+  for (let i = 0; i < data.agents.length; i++) {
+    let template = `
+        <ul>
+          <li>${data.agents.email}</li>
+        </ul>
+     `;
+    document.body.innerHTML = template;
+  }
 }
