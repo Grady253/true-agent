9d34ee7588979b39497ffd4392bfac58ea58fa4b
Grady253 <grady.andre2@gmail.com>
2023-04-13 10:07:47 -0700

    Got agents picture to display.

--- Assests/server.js
+++ Assests/server.js
@@ -33,10 +33,10 @@ function displayData(data) {
   for (let i = 0; i < data.agents.length; i++) {
     let divEl = document.createElement("div");
     console.log(data.agents[i]);
-    console.log(data.agents[i].full_name,data.agents[i].email );
     let template = `
     <div class="card" style="width: 18rem;">
       <div class="card-body">
+        <img id='agent-Pic' src="${data.agents[i].photo.href}" class="card-img-top" alt="Agents picture">
         <h5 class="card-title">${data.agents[i].full_name}</h5>
         <p class="card-text">Agent Rating:${data.agents[i].agent_rating}</p>
         <a href="mailto:${data.agents[i].email}" class="btn btn-primary">Contact Me</a>
