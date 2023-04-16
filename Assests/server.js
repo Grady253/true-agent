16d2f50ef2ffc705256171186713cf26b98949d7
Grady253 <grady.andre2@gmail.com>
2023-04-15 19:39:38 -0700

    Added property sold count, and agent rating.

--- Assests/server.js
+++ Assests/server.js
@@ -30,21 +30,23 @@ async function getAgentApi(zipCode) {
 
 function displayData(data) {
   const propDisplayEl = document.querySelector(".property-display");
+  
   for (let i = 0; i < data.agents.length; i++) {
     let divEl = document.createElement("div");
     console.log(data.agents[i]);
     let template = `
     <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <img id='agent-Pic' src="${data.agents[i].photo.href}" class="card-img-top" alt="Agents picture">
-        <h5 class="card-title">${data.agents[i].full_name}</h5>
-        <p class="card-text">Agent Type: ${data.agents[i].agent_type}</p>
-        <p class="card-text">Agent Rating: ${data.agents[i].agent_rating}</p>
-        <a href="mailto:${data.agents[i].email}" class="btn btn-danger">Contact Me</a>
-      </div>
+    <div class="card-body">
+    <img id='agent-Pic' src="${data.agents[i].photo.href}" class="card-img-top" alt="Agents picture">
+    <h4 class="card-title">${data.agents[i].full_name}</h4>
+    <p class="card-text">Agent Type: ${data.agents[i].agent_type}</p>
+    <p class="card-text">Properties sold: ${data.agents[i].recently_sold.count}</p>
+    <p class="card-text">Agent Rating: ${data.agents[i].agent_rating}</p>
+    <a href="mailto:${data.agents[i].email}" class="btn btn-danger">Contact Me</a>
     </div>
-     `;
-     divEl.innerHTML = template;
+    </div>
+    `;
+    divEl.innerHTML = template;
     propDisplayEl.append(divEl);
   };
 }
